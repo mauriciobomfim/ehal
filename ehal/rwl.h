02f9ca6dd3861e78ee8e2b89441ae4169e4986da
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2010-12-29 03:03:49 +0800

    rearranged identation. Changed doxygen

--- ehal/rwl.h
+++ ehal/rwl.h
@@ -1,12 +1,3 @@
-#ifndef __RWL_H_
-#define __RWL_H_
-
-#ifndef CPU_SIZE
-#warning CPU_SIZE undefined, defaulting to 8 bits
-#define CPU_SIZE	8
-#define CPU_TYPE	unsigned char
-#endif
-
 /** @file
  * Read write lock with one writer or multiple readers.
  * 
@@ -17,12 +8,16 @@
  *
  * @defgroup rwl read/write lock
  * @{  */
+#ifndef EHAL_RWL
+#define EHAL_RWL
+
+#include "cpu.h"
+
 
 /** What this macro means is that the processor word size in bits minus
  * the the other 2 bits in the struct. This was done to make it fit on
- * a register 
- */
-#define RWL_RLOCK_BITS	(CPU_SIZE-2)	/* -2 bits of the structure */
+ * a register */
+#define RWL_RLOCK_BITS	(CPU_SIZE-2)
 
 /** Max number of simultaneous readers. */
 #define RWL_MAX_RLOCKS	(-1U & ( (1<<RWL_RLOCK_BITS)-1 ) )
@@ -35,39 +30,40 @@ enum RWL_RETURN {
 	RWL_WWANTLOCK		/**< Write pending ( rwl_r only ) */
 };
 
-/** Read write structure. */
+/** internal usage structure. */
 struct rwl;
 
 /** Initializes the rwl struct
- * @arg l - rwl pointer
- * @return None */
-void rwl_init (struct rwl *l);
+ * @param l - rwl pointer */
+void
+	rwl_init	(struct rwl *l);
 
 /** Try to get a read lock.
- * @arg l - rwl pointer
- * @return #RWL_RETURN
+ * @param l - rwl pointer
  * @see rwl_runlock */
-enum RWL_RETURN rwl_rlock (struct rwl *l);
+enum RWL_RETURN
+	rwl_rlock	(struct rwl *l);
 
 /** Perform a unlock.
  *
  * unlock when reach 0 readers.
- * @arg l - rwl pointer
- * @return None 
+ * @param l - rwl pointer
  * @see rwl_rlock */
-void rwl_runlock (struct rwl *l);
+void
+	rwl_runlock	(struct rwl *l);
 
 /** Try to get a write lock.
- * @arg l - rwl pointer
+ * @param l - rwl pointer
  * @return #RWL_RETURN
  * @see rwl_wunlock */
-enum RWL_RETURN rwl_wlock (struct rwl *l);
+enum RWL_RETURN
+	rwl_wlock	(struct rwl *l);
 
 /** Perform a unlock.
- * @arg l - rwl pointer
- * @return None
+ * @param l - rwl pointer
  * @see rwl_wlock */
-void rwl_wunlock (struct rwl *l);
+void
+	rwl_wunlock	(struct rwl *l);
 
 /** @} */ /* end rwl group */
 
