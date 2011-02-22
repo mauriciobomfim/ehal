29ee1a81ae55bf426cef00cc5bce10c9122ed7e4
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2011-02-23 06:55:13 +0800

    Added the custom named types.

---
+++ ehal/tinytypes.h
@@ -0,0 +1,18 @@
+#ifndef TINYTYPES_H
+#define TINYTYPES_H
+
+#include <stdbool.h>
+#include <inttypes.h>
+
+/* custom types */
+typedef uint8_t		u08;
+typedef uint16_t	u16;
+typedef uint32_t	u32;
+typedef uint64_t	u64;
+
+typedef int8_t		s08;
+typedef int16_t		s16;
+typedef int32_t		s32;
+typedef int64_t		s64;
+
+#endif
