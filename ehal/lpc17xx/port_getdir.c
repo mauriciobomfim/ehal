a10fe12fe0c1e01034f0a3cfc0b03d0b18250773
Thiago Naves <naves.thiago@gmail.com>
2011-03-18 07:02:07 +0800

    Added hardware mask for gpio in lpc17xx

--- ehal/lpc17xx/port_getdir.c
+++
@@ -1,9 +0,0 @@
-#include "port.h"
-
-#define port_mask(p, m, v) do { p = ((p) & ~(m)) | ((v) & (m)); } while(0)
-
-port_t port_getdir (void *_p)
-{
-	struct port_mem_block *p = _p;
-	return ~p->dir;
-}
